e565e386301c8ccb500864f10c7b9d5fb3224213
Mercynenoh <mercychepngenoh74@gmail.com>
2022-02-25 16:37:41 +0300

    add all files

---
+++ index.js
@@ -0,0 +1,13 @@
+//index.js
+//var number = 5;
+
+//let name = "Mercy "
+
+ // document.writeln(name+number)
+ let (hasJob) = true;
+ if (hasJob) {
+     document.writeln("Thank you for stopping by but I have a job");
+     }
+     else{
+         document.writeln("I am looking for a job reach out for any position")
+     }
